bedc971c94a6841c58364506c72eaf60484aa4c8
Maciel Suassuna <macielsuassuna14@gmail.com>
2024-09-04 17:26:29 -0300

    Back to back

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Você deve estar vendo isso em produção! Pipeline CI/CD implementada + certificado SSL (Laravel | Github | AWS)';
+    return view('welcome');
 });
